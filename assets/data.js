0da21cb1c9a36cec123947e5ecc31271f61dacba
Miguel <miguel@MacBook-Pro-de-Miguel.local>
2021-11-20 00:05:07 +0100

    display products, homepage and product pagae

---
+++ assets/data.js
@@ -0,0 +1,163 @@
+export const largeCardSections = [
+    {
+      id: 1,
+      title: "Top-rated Vacuum Cleaners in Barcelona",
+      snippet: "The bagless canister that collects all the debris and dust detaches from the rest of the vacuum, so you empty it over a garbage.",
+      cards: [
+        {
+          id: 19,
+          title: "ONEPWR EVOLVE Cordless Upright",
+          snippet: "Very versatile and did a good job suctioning up dust and hair on both surfaces.",
+          image: "vc1.jpg"
+        },
+        {
+          id: 20,
+          title: "WindTunnel 3 High Performance",
+          snippet: "This is one of the most red fire extinguishers you will ever see. Super clean.",
+          image: "vc2.jpg"
+        },
+        {
+          id: 21,
+          title: "Oreck Elevate Command Bagged Upright",
+          snippet: "Mechanism is a huge perk: It glides smoothly, makes it super-easy around corners. ",
+          image: "vc3.jpg"
+        }
+      ]
+    },
+    {
+      id: 2,
+      title: "Best Vacuum Cleaners for All Types of Homes",
+      snippet: "Vestibulum accumsan orci in arcu eleifend bibendum. Morbi malesuada dictum suscipit.",
+      cards: [
+        {
+          id: 22,
+          title: "Bissell Eraser Turbo Upright",
+          snippet: "Ease of use, though, is where the Elevate Command really shines. Its simple.",
+          image: "vc4.jpg"
+        },
+        {
+          id: 23,
+          title: "Dyson Animal Ball 2",
+          snippet: "This even on the less powerful hard-floor setting, I couldn’t really use the vacuum.",
+          image: "vc5.jpg"
+        },
+        {
+          id: 24,
+          title: "Eureka Powerspeed Turbo",
+          snippet: "In a world where vacuums attempt to do more and more (and look less and less like vacuums.",
+          image: "vc6.jpg"
+        }
+      ]
+    }
+  ]
+
+  export const smallCardSections = [
+      {
+          id: 1,
+          title: "Top-rated Vacuum Cleaner in Bilbao",
+          cards: [
+              {
+                id: 1,
+                image: "vc7.jpg"
+              },
+              {
+                id: 2,
+                image: "vc8.jpg"
+              },
+              {
+                id: 3,
+                image: "vc9.jpg"
+              },
+              {
+                id: 4,
+                image: "vc10.jpg"
+              },
+              {
+                id: 5,
+                image: "vc11.jpg"
+              },
+              {
+                id: 6,
+                image: "vc12.jpg"
+              },
+              {
+                id: 7,
+                image: "vc13.jpg"
+              },
+              {
+                id: 8,
+                image: "vc14.jpg"
+              },
+              {
+                id: 9,
+                image: "vc15.jpg"
+              },
+              {
+                id: 10,
+                image: "vc16.jpg"
+              },
+              {
+                id: 11,
+                image: "vc17.jpg"
+              },
+              {
+                id: 12,
+                image: "vc18.jpg"
+              }
+          ]
+      },
+      {
+        id: 2,
+        title: "Top-rated Vacuum Cleaners in Valencia",
+        cards: [
+            {
+              id: 13,
+              image: "vc19.jpg"
+            },
+            {
+              id: 14,
+              image: "vc20.jpg"
+            },
+            {
+              id: 15,
+              image: "vc21.jpg"
+            },
+            {
+              id: 16,
+              image: "vc22.jpg"
+            },
+            {
+              id: 17,
+              image: "vc23.jpg"
+            },
+            {
+              id: 18,
+              image: "vc24.jpg"
+            },
+            {
+              id: 19,
+              image: "vc1.jpg"
+            },
+            {
+              id: 20,
+              image: "vc2.jpg"
+            },
+            {
+              id: 21,
+              image: "vc3.jpg"
+            },
+            {
+              id: 22,
+              image: "vc4.jpg"
+            },
+            {
+              id: 23,
+              image: "vc5.jpg"
+            },
+            {
+              id: 24,
+              image: "vc6.jpg"
+            }
+        ]
+    }
+  ]
---
+++ components/Hero.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="Hero container">
+        <div class="text-container">
+            <h1 class="header">Find your Vacuum Cleaner</h1>
+            <p class="snippet">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste iusto illum quidem asperiores, cum nisi aperiam eius ipsa sapiente odit!</p>
+            <button class="button btn">Start Looking</button>
+        </div>
+        <span class="circle"></span>
+        <img class="image" src="~assets/svg/vacuum-cleaner.svg" alt="fire extinguisher">
+    </div>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style scoped>
+    .Hero {
+        height: 600px;
+        position: relative;
+        display: flex;
+        padding: 5rem 0;
+        align-items: flex-start;
+        justify-content: space-between;
+    }
+    .container{}
+    .text-container {
+        width: 50%;
+        margin-top: 4rem;
+    }
+    .header {
+        font-weight: 900;
+        font-size: 5rem;
+    }
+    .snippet {
+        color: grey;
+        font-size: 1.25rem;
+    }
+    .image {
+        width: 22rem;
+        margin-top: 4.8rem;
+        z-index: 2;
+    }
+    .circle {
+        border-radius: 50%;
+        width: 22rem;
+        height: 22rem;
+        background-color: rgb(231, 81, 43);
+        position: absolute;
+        right:70px;
+        top:170px;
+        z-index: 1;
+    }
+    .overlay {
+        background: rgba(0, 0, 0, 0);
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        top: 0;
+    }
+    .button {
+        padding: 1rem 4rem;
+        border-radius: 100rem;
+        background-color: rgb(231, 81, 43);
+        color: white;
+        width: 20rem;
+        font-weight: 700;
+        transition: 0.5s;
+    }
+    .button:hover {
+        width: 21.5rem;
+    }
+    @media (max-width: 500px) {
+        .Hero {
+            height: 40vh;
+        }
+    }
+</style>
